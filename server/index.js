7191d3a853de7ec82ea109f7406d082ca3a801ba
LeandroLicata <leandrolicata1@gmail.com>
2023-03-11 16:46:10 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import morgan from "morgan";
+import { Server as SocketServer } from "socket.io";
+import http from "http";
+import cors from "cors";
+import { PORT } from "./config.js";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname)
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+app.use(cors());
+app.use(morgan("dev"));
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("message", (message) => {
+    socket.broadcast.emit("message", {
+      body: message,
+      from: socket.id,
+    });
+  });
+});
+
+app.use(express.static(join(__dirname, "../client/build")));
+
+server.listen(PORT);
+console.log("server started on port ", PORT);
